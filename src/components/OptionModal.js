710f51d39a135817bbe04223b149a8bedf4c9ace
Brian Bycynski <high5bri@hotmail.com>
2018-02-07 06:32:26 -0500

    finished

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -8,10 +8,18 @@ const OptionModal = props => (
         onRequestClose={props.handleClearSelectedOption}
         contentLabel="Selected Option"
         ariaHideApp={false}
+        closeTimeoutMS={200}
+        className="modal"
     >
-        <h3 style={{ textAlign: 'center' }}>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleClearSelectedOption}>OKAY</button>
+        <h3 className="modal__title" style={{ textAlign: 'center' }}>
+            Selected Option
+        </h3>
+        {props.selectedOption && (
+            <p className="modal__body">{props.selectedOption}</p>
+        )}
+        <button className="button" onClick={props.handleClearSelectedOption}>
+            OKAY
+        </button>
     </Modal>
 );
 
